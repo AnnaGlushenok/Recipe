5ea68d23ea3eeb610f6984bcdc00d28b8853a66e
Anna <annasan14052003@gmail.com>
2025-11-11 22:54:09 +0300

    clean

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -2,11 +2,7 @@ import Recipe from "@/components/RecipeComponent/Recipe";
 
 export default async function RecipePage({params}: { params: Promise<{ id: string }> }) {
     const {id} = await params
-    return (
-        <div>
-            <Recipe id={+id}/>
-        </div>
-    );
+    return <Recipe id={+id}/>;
 }
 
 export const dynamic = "force-dynamic";
