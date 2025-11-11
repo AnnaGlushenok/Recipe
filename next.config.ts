165e18f9cec8ea84e5e83d8ff62bb188aee33717
Anna <annasan14052003@gmail.com>
2025-11-11 22:53:36 +0300

    added images configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "msnnmlrran8m6azp.public.blob.vercel-storage.com",
+            },
+        ],
+    },
 };
-
 export default nextConfig;
