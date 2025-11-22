cdc770a0577c2ebb6bb2bd6049b33d3ec909c42e
annas <annasan14052003@gmail.com>
2025-11-22 19:04:09 +0300

    changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type {Metadata} from "next";
-import {Cormorant_Garamond} from "next/font/google";
+import {EB_Garamond} from "next/font/google";
 import "./globals.css";
 import "./normalize.css";
 
-const cormorantGaramond = Cormorant_Garamond({
-    variable: "--font-cormorant-garamond",
+const ebGaramond = EB_Garamond({
+    variable: "--font-eb-garamond",
     weight: ['400', '500', '600', '700'],
     subsets: ["cyrillic"],
 });
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="ru">
-        <body className={`${cormorantGaramond.variable} antialiased min-h-screen`}>
+        <body className={`${ebGaramond.variable} antialiased min-h-screen`}>
         {children}
         </body>
         </html>
